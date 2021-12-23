65dc1b22967f0a6b28123d72726a1ce148040bce
xxw <xxxx.con>
2021-12-23 17:45:25 +0800

    Request开始学习

---
+++ test_code/Request.py
@@ -0,0 +1,22 @@
+import unittest
+from functools import wraps
+
+def Test(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("=========测试 ", func.__name__)
+        return func(*args, **kwargs)
+    return wrapper
+
+class RequestTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    @Test
+    def test_1(self):
+        print("aaaaaaaaa")
+
